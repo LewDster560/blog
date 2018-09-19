73c5566249e20053f4dae2a0765fcef2f138ea02
liudong <416553689>
2018-09-19 15:25:03 +0800

    解决mapper报错，增加扫描路径

--- src/main/java/com/springboot/blog/controller/IndexController.java
+++ src/main/java/com/springboot/blog/controller/IndexController.java
@@ -1,23 +1,37 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.utils.RedisUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Controller
+@Api(description = "首页控制器", tags = "首页控制器", value = "首页控制器")
 public class IndexController {
 
     @Autowired
     RedisUtil redisUtil;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/")
+    @ApiOperation(value = "首页")
     public String index() {
         log.info("-------------------------------->这是首页");
         return "index";
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/springboot/blog/mapper/StudentMapper.java
+++ src/main/java/com/springboot/blog/mapper/StudentMapper.java
@@ -2,15 +2,19 @@ package com.springboot.blog.mapper;
 
 import com.springboot.blog.entity.Student;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author liudong
  * @since 2018-09-19
  */
+@Mapper
 public interface StudentMapper extends BaseMapper<Student> {
 
 }
