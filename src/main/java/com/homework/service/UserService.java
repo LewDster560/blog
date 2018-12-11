766391288f138043e55271ad29cb88a6de260e27
liudong <416553689>
2018-12-11 17:28:19 +0800

    1.pom引入shiro,验证码 2.shiro,验证码配置 3.登录注册

--- src/main/java/com/homework/service/UserService.java
+++ src/main/java/com/homework/service/UserService.java
@@ -1,14 +1,16 @@
 package com.homework.service;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.api.R;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.homework.entity.User;
+import com.homework.shiro.AccountProfile;
 
 import java.util.Map;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author lv-success
@@ -18,8 +20,27 @@ public interface UserService extends IService<User> {
 
     /**
      * 给关联用户的分页结果添加用户信息
+     *
      * @param pageData
      * @param linkfield
      */
     void join(IPage<Map<String, Object>> pageData, String linkfield);
+
+    /**
+     * 用于用户登录
+     * AccountProfile是有用户基本信息的类，包括私信、通知数量，头像等
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    AccountProfile login(String email, String password);
+
+    /**
+     * 注册
+     *
+     * @param user
+     * @return
+     */
+    R register(User user);
 }
